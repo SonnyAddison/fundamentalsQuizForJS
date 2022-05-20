e0531a743b7967ea1954266d08764d9e05176a08
Sonny Addison <sonny.m.addison@hotmail.com>
2022-05-19 22:51:14 -0700

    Updated Code to html, and css

---
+++ assets/script.js
@@ -0,0 +1,78 @@
+
+
+
+
+
+
+
+
+
+/*
+<section id='quest1'>
+    <h1>JavaScript is</h1>
+    <button>double-threaded</button>
+    <button>dynamic language</button>
+    <button>non-supporting</button>
+    <button>gadget-oriented</button>
+</section>
+<section id='quest2'>
+    <h1 >Math.random(x) returns a whole number between</h1>
+    <button>0 and x-2</button>
+    <button>-1 and x-1</button>
+    <button>0 and x-0</button>
+    <button>0 and x-1</button>
+</section>
+<section id='quest3'>
+    <h1>A variable is a container for a</h1>
+    <button>value</button>
+    <button>name</button>
+    <button>place</button>
+    <button>container</button>
+</section>
+<section id='quest4'>
+    <h1>To help stop hoisting JavaScript prefers the use of what instead of var?</h1>
+    <button>help</button>
+    <button>hoisting</button>
+    <button>let</button>
+    <button>const</button>
+</section>
+<section id='quest5'>
+    <h1>True or False a const can be changed through reassignment?</h1>
+    <button>True</button>
+    <button>False</button>
+</section>
+<section id='quest6'>
+    <h1>Ture or False: const, let, var are all the same thing?</h1>
+    <button>True</button>
+    <button>False</button>
+</section>
+<section id='quest7'>
+    <h1>How many Primitive Values are there in JavaScript?</h1>
+    <button>7</button>
+    <button>6 </button>
+    <button>8</button>
+    <button>15</button>
+</section>
+<section id='quest8'>
+    <h1>Objects are a primitive value</h1>
+    <button>Ture</button>
+    <button>False</button>
+</section>
+<section id='quest9'>
+    <h1>NaN stands for</h1>
+    <button>Now and Null</button>
+    <button>Null and Null</button>
+    <button>Not a Numberical</button>
+    <button>Not a Number</button>
+</section>
+<section id='quest10'>
+    <h1>In JavaScript, closures are created/needed every time a function is created?</h1>
+    <button>True</button>
+    <button>False</button>
+</section>
+<section id='quest11'>
+    <h1>Arguments are/is an Array-like objet that is accessible inside what?</h1>
+    <button>arrays</button>
+    <button>features</button>
+    <button>elements</button>
+    <button>functions</button> */
